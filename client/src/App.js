15454675ccee7a3f83b52dc01e804f944c2e4a12
Chloe Harrison <chloesian.harrison@live.co.uk>
2024-07-02 17:39:25 +0100

    landing page created

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>Hello</div>
+    <main>
+      <header>
+        <h1 className="logo">TellIt</h1>
+        <nav>
+          <a href="#">Discover</a>
+          <a href="#">About Us</a>
+          <a href="#">Login</a>
+          <a href="#">Sign Up</a>
+        </nav>
+      </header>
+
+      <div className="image">
+      </div>
+    </main>
   );
 }
 
